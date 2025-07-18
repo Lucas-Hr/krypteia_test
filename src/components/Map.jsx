7f31fd959bc8da60abbef6209b81f76bae21a150
Lucas-Hr <lucashenintsoarandriamiarisoa@gmail.com>
2025-07-18 21:26:49 +0300

    feat : Ajout bouton Switch

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -3,29 +3,36 @@ import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import { useBrewery } from '../context/breweryContext'
 import Card from './Card';
 import 'leaflet/dist/leaflet.css';
-
+import MapPin from '../assets/MapPin';
 
 const Map = () => {
     const {brewery, setBrewery} = useBrewery();
   return (
-    <MapContainer center={[35.25738891, -97.46818222]} zoom={13} scrollWheelZoom={false} style={{width : "60vw", height : "60vh"}} className='absolute top-[200px] left-[300px]' >
-        <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        {
-            brewery.map((b, index) => {
-                return (
-                    <Marker key={index} position={[`${Number(b.latitude) ?? 0}` , `${Number(b.longitude) ?? 0}`]}>
-                        <Popup style={{width : 200, height : 50}}>
-                            <Card name={b.name} brewery_type={b.brewery_type} country={b.country} state={b.state} street={b.street} phone={b.phone} website_url={b.website_url ?? "Unknown"}/>
-                        </Popup>
-                    </Marker>
-                )
-            })
-        }
-        
-    </MapContainer>
+    <div className='relative mt-12'>
+        <div className='bg-white rounded-sm flex items-center px-4 py-2 absolute z-20 top-[20px] right-[100px]'> 
+                <MapPin />
+                <p className='ms-2 text-sm font-semibold'>Brewery</p>
+        </div>
+        <MapContainer center={[35.25738891, -97.46818222]} zoom={5} scrollWheelZoom={false} style={{width : "90vw", height : "80vh"}} className='relative  z-10' >
+            <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            {
+                brewery.map((b, index) => {
+                    return (
+                        <Marker key={index} position={[`${Number(b.latitude) ?? 0}` , `${Number(b.longitude) ?? 0}`]}>
+                            <Popup>
+                                <Card name={b.name} brewery_type={b.brewery_type} country={b.country} state={b.state} city={b.city} street={b.street} phone={b.phone} website_url={b.website_url ?? "Unknown"}/>
+                            </Popup>
+                        </Marker>
+                    )
+                })
+            }
+            
+        </MapContainer>
+    </div>
+    
 
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -9,5 +9,5 @@ createRoot(document.getElementById('root')).render(
     <BreweryProvider>
       <App />
     </BreweryProvider>
-  </StrictMode>,
+  </StrictMode>
 )
